2367974977ae3b2be92e15f14f27309e5a6c02a1
Farid0558475484 <faridmh@code.edu.az>
2023-04-27 02:28:24 +0400

    feat:add expressjs and nodemon

--- app.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("./test.txt", "utf8", (error, data) => {
-  fs.watchFile("./test2.txt", `${data} New text`, () => {
-    console.log(data);
-  });
-
-  console.log(data);
-});
--- modules.js
+++
@@ -1,5 +0,0 @@
-const { userName, sayHi } = require("./global");
-
-const name = "Elnar";
-
-console.log(sayHi(userName));
