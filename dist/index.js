668e35811ee94eb1a74dd4d36a413fa779390491
Farid0558475484 <faridmh@code.edu.az>
2023-04-27 03:24:38 +0400

    feat:add ts

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/user", (req, res) => {
+    res.send("Hello User!");
+});
+app.post("/user", (req, res) => {
+    res.send("Created Users!");
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
